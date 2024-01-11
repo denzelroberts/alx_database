18e9cf0ef3bf31d461cb484948301ec634e2a7d8
denzelroberts <iamdenzelroberts@gmail.com>
2024-01-11 19:45:18 +0000

    read user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- A script that creates the database hbtn_0d_2 and the user user_0d_2.
+
+-- creating the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- creating the user with password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+-- the user has select privileges in hbtn_0d_2
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
